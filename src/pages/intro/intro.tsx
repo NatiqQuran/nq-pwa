9b1938210903309494b78f2ac898cb17c281bafa
AzerothA <telifesite@gmail.com>
2022-09-15 14:41:50 +0430

    New update

--- src/pages/intro/intro.tsx
+++ src/pages/intro/intro.tsx
@@ -1,20 +1,26 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { Header } from "../../components/components";
 import useFetch from "../../useFetch";
 
 function Intro() {
     const [button, setButton] = useState<"back" | "menu">("menu");
 
-    const { body, send, response } = useFetch("http://127.0.0.1:8080/status");
+    const { body, send, response, error, loading } = useFetch(
+        "http://127.0.0.1/status"
+    );
 
     return (
         <div>
             <Header title="HelloWorld" button={button}>
                 <button onClick={send}>Button</button>
             </Header>
+            <h1>{loading ? "loading" : "NULL"}</h1>
+            <br />
             <h1>{body ? (body as string) : "Not found"}</h1>
             <br />
             <h2>Status Code: {response?.status}</h2>
+            <br />
+            <h4>If any Error: {error?.message}</h4>
         </div>
     );
 }

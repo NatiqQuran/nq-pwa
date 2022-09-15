af7f2ef11ba5999b34e49cdb7c94d6a6c1044bfc
AzerothA <telifesite@gmail.com>
2022-09-15 10:50:28 +0430

    New Update to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { ReactComponent as Humburger } from "../../assets/svg/humburger.svg";
 import "./header.css";
 
 interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
     title: string;
+    button: "menu" | "back" | null;
 }
 
 function Header(props: HeaderProps) {
     return (
         <header>
-            {"Icon"}
+            <button>
+                {props.button === "menu" ? (
+                    <Humburger />
+                ) : props.button === "back" ? (
+                    "BACK"
+                ) : null}
+            </button>
             <h1 style={{ marginInlineEnd: "auto" }}>{props.title}</h1>
 
             {props.children}

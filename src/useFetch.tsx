ea9499d2a0e8fb0b5c96e4543b4a80d84516a4c7
AzerothA <telifesite@gmail.com>
2022-09-15 14:41:59 +0430

    Updated

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-// Custom Hook
-// For handling Fetch
-function useFetch(url: string) {
-    const [response, setResponse] = React.useState<Response>();
-    const [body, setBody] = React.useState<object | string>();
+/**
+ * @description Custom Hook For handling Fetch
+ * @version 0.1
+ */
+function useFetch(url: string, init?: RequestInit) {
+    const [response, setResponse] = useState<Response>();
+    const [body, setBody] = useState<object | string>();
+    const [error, setError] = useState<null | Error>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    const send = () => fetch(url, {}).then((response) => setResponse(response));
+    const send = () => {
+        setLoading(true);
+        fetch(url, init)
+            .then((response) => setResponse(response))
+            .catch((error) => setError(error));
+        setLoading(false);
+    };
 
     useEffect(() => {
         response
@@ -16,7 +26,7 @@ function useFetch(url: string) {
             .catch(() => response.text().then((string) => setBody(string)));
     }, [response]);
 
-    return { response, send, body };
+    return { response, send, body, error, loading };
 }
 
 export default useFetch;

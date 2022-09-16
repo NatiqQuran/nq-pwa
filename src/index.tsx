dfbd6302e235ec6d3fe3e43056bbb1eabe9219da
nexnema <nexnema@gmail.com>
2022-09-16 14:56:59 +0430

    separate router

--- src/index.tsx
+++ src/index.tsx
@@ -2,49 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Pwa, Intro, Quran } from "pages";
+import Router from "./router";
 import "./assets/css/style.css";
 import "./assets/css/color.css";
 import "./assets/css/form.css";
 import "./assets/css/scrollbar.css";
-
-const isPwaIntroPagePassed = (): boolean => {
-    return localStorage.getItem("pwaIntroPassed") === "true";
-};
-
-//Test mode in local host
-const isLocalhost = Boolean(
-    window.location.hostname === "localhost" ||
-        // [::1] is the IPv6 localhost address.
-        window.location.hostname === "[::1]" ||
-        // 127.0.0.0/8 are considered localhost for IPv4.
-        window.location.hostname.match(
-            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-        )
-);
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Intro />} />
-                <Route
-                    path="/pwa"
-                    element={
-                        isPwaIntroPagePassed() && !isLocalhost ? (
-                            <Navigate replace to="/quran" />
-                        ) : (
-                            <Pwa />
-                        )
-                    }
-                />
-                <Route path="/quran" element={<Quran />} />
-            </Routes>
-        </BrowserRouter>
+        <Router />
     </React.StrictMode>
 );
 

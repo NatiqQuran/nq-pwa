1e89a56cb0553aac409b204712890b0f73958ea1
AzerothA <telifesite@gmail.com>
2022-09-19 12:19:20 +0430

    OnClick prop added

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,17 +6,18 @@ import "./header.css";
 interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
     title: string;
     button: "menu" | "back" | null;
+    buttonOnClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 function Header(props: HeaderProps) {
     return (
         <header>
             {props.button === "menu" ? (
-                <button>
+                <button onClick={props.buttonOnClick}>
                     <HumburgerIcon />
                 </button>
             ) : props.button === "back" ? (
-                <button>
+                <button onClick={props.buttonOnClick}>
                     <BackIcon />
                 </button>
             ) : null}

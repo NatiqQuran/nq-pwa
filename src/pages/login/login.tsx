d14077f1c4cb498efd059c9eda07746815c9c5a2
AzerothA <telifesite@gmail.com>
2022-10-01 11:21:39 +0330

    Unused function import removed

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useFetch } from "library";
 import { Button } from "ui";
 

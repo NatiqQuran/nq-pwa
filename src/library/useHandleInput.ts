c2be7abe9724ce5a35d8b985e44549af4bc8c96f
AzerothA <telifesite@gmail.com>
2022-10-02 15:21:49 +0330

    New Update (TypeCheck)

--- src/library/useHandleInput.ts
+++ src/library/useHandleInput.ts
@@ -1,8 +1,23 @@
+import React from "react";
+
+/**
+ * Handles all the inputs with one function,
+ * Append the name and value of the input to the given state
+ * @param stateSetter setter of the state
+ */
 function useHandleInput<T>(
     stateSetter: React.Dispatch<React.SetStateAction<T>>
 ) {
-    const handleInput = (e: any) =>
-        stateSetter((state) => ({ ...state, [e.target.name]: e.target.value }));
+    const append = (newValue: T) =>
+        stateSetter((state) => ({ ...state, ...newValue }));
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.type === "number") {
+            append({ [e.target.name]: parseInt(e.target.value, 10) } as T);
+        } else {
+            append({ [e.target.name]: e.target.value } as T);
+        }
+    };
 
     return { handleInput };
 }

54efd8e8ed1dcf3d2acaa9e1ec4a6260fafd850e
AzerothA <telifesite@gmail.com>
2022-10-17 13:54:51 +0330

    unused imports removed

--- src/library/useFetch.ts
+++ src/library/useFetch.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 /**
  * @description Custom Hook For handling Fetch
--- src/routes/quran/index.tsx
+++ src/routes/quran/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useTransition } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useFetch, useMedia } from "library";
 import {

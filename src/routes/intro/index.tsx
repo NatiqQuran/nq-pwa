2de2aedabcef70cc5a113941b20e8babf992efdf
erfanekrami <erfanak964@gmail.com>
2022-10-19 16:20:54 +0330

    fix problem

--- src/routes/intro/index.tsx
+++ src/routes/intro/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Main, Button, Page, Spacer, Stack } from "ui";
-import { ReactComponent as Menu } from "../../assets/svg/menu.svg";
-import { ReactComponent as SearchIcon } from "../../assets/svg/search.svg";
 
 function Intro() {
     return (
         <Page>
             <AppBar>
-                <Button icon={<Menu />} />
                 <h1>Natiq</h1>
                 <Spacer />
                 <Link to="/quran">
@@ -20,7 +17,6 @@ function Intro() {
                 <Link to="/account/login">
                     <Button>login</Button>
                 </Link>
-                <Button icon={<SearchIcon />} />
             </AppBar>
 
             <Main>

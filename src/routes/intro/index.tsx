162457368e370b9f721d1eb777a9b53e79dcfbe5
erfanekrami <erfanak964@gmail.com>
2022-10-23 23:49:43 +0330

    test dropdown

--- src/routes/intro/index.tsx
+++ src/routes/intro/index.tsx
@@ -1,11 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useFetch, useMedia } from "library";
 import { Link } from "react-router-dom";
-import { AppBar, Main, Button, Page, Spacer, Stack } from "ui";
+import {
+    AppBar,
+    Main,
+    Button,
+    Page,
+    Spacer,
+    Stack,
+    ListItem,
+    List,
+    ClickAwayListener,
+    Navigation,
+    SvgIcon,
+} from "ui";
+import { ReactComponent as Menu } from "../../assets/svg/menu.svg";
+
+function NavigationList() {
+    const [collapsedList, setcollapsedList] = React.useState(true);
+
+    const handleClick = () => {
+        setcollapsedList(!collapsedList);
+    };
+
+    return (
+        <List direction="column">
+            <ListItem>
+                <Button
+                    size="medium"
+                    icon={<Menu />}
+                    borderStyle="semi"
+                    onClick={handleClick}
+                >
+                    <span>Quran</span>
+                    <Spacer />
+                    <span style={{ color: "#7d7d7d", fontSize: "1.4rem" }}>
+                        14:106
+                    </span>
+                </Button>
+                <List collapsed={collapsedList}>
+                    <ListItem>
+                        <Button size="medium" borderStyle="semi">
+                            <span>Cantact</span>
+                        </Button>
+                    </ListItem>
+                </List>
+            </ListItem>
+            <ListItem>
+                <Button size="medium" borderStyle="semi">
+                    <span>Quran</span>
+                    <Spacer />
+                </Button>
+            </ListItem>
+        </List>
+    );
+}
 
 function Intro() {
+    const [navOpen, setNavOpen] = useState<boolean>(false);
+    const matches = useMedia("(max-width: 1000px)");
+    const toggleNavOpen = () => setNavOpen(value => !value);
     return (
         <Page>
             <AppBar>
+                <Button
+                    icon={<Menu />}
+                    onClick={toggleNavOpen}
+                    variant="tonal"
+                />
                 <h1>Natiq</h1>
                 <Spacer />
                 <Link to="/quran">
@@ -19,11 +81,23 @@ function Intro() {
                 </Link>
             </AppBar>
 
-            <Main>
+            <Main navOpen={navOpen}>
                 <div>
                     <h1>Hello World</h1>
                 </div>
             </Main>
+
+            {matches ? (
+                <ClickAwayListener onClickAway={() => setNavOpen(false)}>
+                    <Navigation open={navOpen}>
+                        <NavigationList />
+                    </Navigation>
+                </ClickAwayListener>
+            ) : (
+                <Navigation open={navOpen}>
+                    <NavigationList />
+                </Navigation>
+            )}
         </Page>
     );
 }
--- src/ui/list/list.tsx
+++ src/ui/list/list.tsx
@@ -4,12 +4,14 @@ import styles from "./list.module.css";
 
 interface ListProps extends React.HTMLAttributes<HTMLUListElement> {
     direction?: "row" | "column";
+    collapsed?: boolean;
 }
 
 function List(props: ListProps) {
     const joinedClassNames = joinClassNames(
         styles.list,
         props.direction ? styles[props.direction] : styles.row,
+        props.collapsed ? styles.collapsed : "",
         props.className!
     );
 

791893a29a0284d083ff66c652ba090c93668539
erfanekrami <erfanak964@gmail.com>
2023-04-12 17:06:47 +0330

    fix problem, add bismillah

--- src/routes/quran/index.tsx
+++ src/routes/quran/index.tsx
@@ -28,17 +28,28 @@ interface Surah {
     id: number;
     name: string;
     period: string;
+    bismillah_status: "in_ayah" | "true" | "false";
+    bismillah_text: string | null;
+
     ayahs: Verse[];
 }
+
+const Ayah = (props: { ayah: Verse }) => (
+    <span>
+        {props.ayah.content.text}
+        <span> {toArabic(props.ayah.number)} </span>
+    </span>
+);
+
+const toArabic = (input: any) => input.toLocaleString("ar-EG");
 
 function Quran() {
     const [navOpen, setNavOpen] = useState<boolean>(false);
     const matches = useMedia("(max-width: 1000px)");
     const toggleNavOpen = () => setNavOpen(value => !value);
     const { id } = useParams();
-    const surahFetch = useFetch<Surah[]>(
-        process.env.REACT_APP_API_URL +
-            `/quran?from=${id}&limit=${id}&mushaf=hafs&format=ayah`,
+    const surahFetch = useFetch<Surah>(
+        process.env.REACT_APP_API_URL + `/surah/${id}?mushaf=hafs`,
         {
             method: "GET",
         }
@@ -49,7 +60,7 @@ function Quran() {
     }, []);
 
     return (
-        <Page>
+        <Page style={{ minHeight: "100vh" }}>
             <AppBar style={{ gap: "0" }}>
                 <Button icon={<Menu />} onClick={toggleNavOpen} />
                 <h1>Quran</h1>
@@ -68,26 +79,51 @@ function Quran() {
                         dir="rtl"
                         style={{ padding: "5px" }}
                     >
-                        <h1 style={{ textAlign: "center", fontFamily: "hafs" }}>
-                            {" "}
-                            {surahFetch.responseBody[0].name}
-                        </h1>
+                        <span
+                            style={{
+                                textAlign: "center",
+                                fontFamily: "hafs",
+                                fontSize: "3rem",
+                            }}
+                        >
+                            <h4>
+                                {surahFetch.responseBody.name}
+                                {surahFetch.responseBody.id}
+                            </h4>
+                            <h2>
+                                {surahFetch.responseBody.bismillah_status ===
+                                "in_ayah"
+                                    ? `${
+                                          surahFetch.responseBody.ayahs[0]
+                                              .content.text
+                                      }${toArabic(
+                                          surahFetch.responseBody.ayahs[0]
+                                              .number
+                                      )}`
+                                    : surahFetch.responseBody
+                                          .bismillah_status === "true"
+                                    ? `${surahFetch.responseBody.bismillah_text}`
+                                    : ""}
+                            </h2>
+                        </span>
 
                         <Stack>
                             <Card
                                 style={{
-                                    fontSize: "2rem",
+                                    fontSize: "2.4rem",
                                     lineHeight: "4rem",
                                     fontFamily: "hafs",
                                     textAlign: "justify",
                                 }}
                             >
-                                {surahFetch.responseBody[0].ayahs.map(ayah => (
-                                    <span>
-                                        {ayah.content.text}{" "}
-                                        <span>[[{ayah.number}]] </span>
-                                    </span>
-                                ))}
+                                {surahFetch.responseBody.bismillah_status ===
+                                "in_ayah"
+                                    ? surahFetch.responseBody.ayahs
+                                          .slice(1)
+                                          .map(ayah => <Ayah ayah={ayah} />)
+                                    : surahFetch.responseBody.ayahs.map(
+                                          ayah => <Ayah ayah={ayah} />
+                                      )}
                             </Card>
                         </Stack>
                     </Container>

f6fc576f2f4b707189cdc07578c5dd6f9efd7199
nexnema <nexnema@gmail.com>
2023-04-22 14:02:47 +0430

    louding better view

--- src/routes/intro/introSurah.tsx
+++ src/routes/intro/introSurah.tsx
@@ -63,40 +63,44 @@ function IntroSurah() {
     }, []);
 
     return (
-        <Stack style={{ width: "100%", alignItems: "center" }}>
-            <span style={{
-                fontSize: "3rem",
-                fontFamily: "Hafs",
-                textAlign: "justify",
-                textAlignLast: "center"
-            }}>
-                {!surahFetch.isResponseBodyReady ? (
-                    "loading..."
-                ) :
-                    surahFetch.responseBody.ayahs.map(
-                        ayah => <Ayah ayah={ayah} />
-                    )
-                }
-            </span>
-            <p
-                style={{
-                    textAlign: "justify",
-                    textAlignLast: "center",
-                    fontSize: "1.6rem",
-                    lineHeight: "2.8rem",
-                }}
-            >
-                In the name of Allah, the Entirely Merciful, the Especially Merciful [1]
-                <br />
-                [All] praise is [due] to Allah, Lord of the worlds [2]
-                The Entirely Merciful, the Especially Merciful [3]
-                Sovereign of the Day of Recompense [4]
-                It is You we worship and You we ask for help [5]
-                Guide us to the straight path [6]
-                The path of those upon whom You have bestowed favor, not of those who have evoked [Your] anger or of those who are astray [7]
-            </p>
-            <audio src={fatihah} controls />
-        </Stack>
+        <div>
+            {!surahFetch.isResponseBodyReady ? (
+                "loading..."
+            ) :
+                (
+                    <Stack style={{ width: "100%", alignItems: "center" }}>
+                        <span style={{
+                            fontSize: "3rem",
+                            fontFamily: "Hafs",
+                            textAlign: "justify",
+                            textAlignLast: "center"
+                        }}>
+                            {surahFetch.responseBody.ayahs.map(
+                                ayah => <Ayah ayah={ayah} />
+                            )}
+                        </span>
+                        <p
+                            style={{
+                                textAlign: "justify",
+                                textAlignLast: "center",
+                                fontSize: "1.6rem",
+                                lineHeight: "2.8rem",
+                            }}
+                        >
+                            In the name of Allah, the Entirely Merciful, the Especially Merciful [1]
+                            <br />
+                            [All] praise is [due] to Allah, Lord of the worlds [2]
+                            The Entirely Merciful, the Especially Merciful [3]
+                            Sovereign of the Day of Recompense [4]
+                            It is You we worship and You we ask for help [5]
+                            Guide us to the straight path [6]
+                            The path of those upon whom You have bestowed favor, not of those who have evoked [Your] anger or of those who are astray [7]
+                        </p>
+                        <audio src={fatihah} controls />
+                    </Stack >
+                )
+            }
+        </div>
     );
 }
 

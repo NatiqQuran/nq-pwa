3dfe1ffbe0e6c33b7df2a13a3b01e4391732af56
sjadk757 <sjadk757@gmail.com>
2023-09-20 10:11:11 +0330

    Delete React

--- src/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-import Intro from "./intro";
-import Pwa from "./pwa";
-import Quran from "./quran";
-import Account from "./account";
-import Search from "./search";
-
-export { Intro, Pwa, Quran, Account, Search };

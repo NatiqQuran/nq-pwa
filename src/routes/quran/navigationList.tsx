bd90ba4be200ad0ea13fe00ac3da08d72cd0f779
al-abd <nexnema@gmail.com>
2024-09-09 17:24:56 +0330

    Better Navigation

--- src/routes/quran/navigationList.tsx
+++ src/routes/quran/navigationList.tsx
@@ -11,22 +11,13 @@ import {
     Stack,
 } from "@yakad/ui";
 
-import { QuranConfigProps } from ".";
 import { getLangNameFromCode } from "../../assets/ts/langCode";
 import { SurahInListProps, TranslationInListProps } from "assets/ts/interface";
+import { QuranConfigProps } from ".";
+import { selectDefaultTranslationUUIDFromList } from "./config";
 
 interface CollapseList {
     [n: number]: boolean;
-}
-
-function selectDefaultTranslationUUIDFromList(
-    translationList: TranslationInListProps[]
-): string {
-    const language = "en";
-    const defaultTranslation = translationList.find(
-        (translation) => translation.language === language
-    );
-    return defaultTranslation ? defaultTranslation.uuid : "No Translation Find";
 }
 
 export default function NavigationList(props: {
@@ -41,12 +32,143 @@ export default function NavigationList(props: {
             [index]: object[index] ? !object[index] : true,
         }));
 
+    const listObjects = [
+        {
+            name: "Quran",
+            navListItems: (
+                <NavListItemsQuran
+                    config={props.config}
+                    setConfig={props.setConfig}
+                />
+            ),
+        },
+        {
+            name: "Arabic Text",
+            navListItems: (
+                <NavListItemsArabicText
+                    config={props.config}
+                    setConfig={props.setConfig}
+                />
+            ),
+        },
+        {
+            name: "Translation",
+            navListItems: (
+                <NavListItemsTranslation
+                    config={props.config}
+                    setConfig={props.setConfig}
+                />
+            ),
+        },
+    ];
+
+    return (
+        <List direction="column">
+            {listObjects.map((item, index) => (
+                <ListItem>
+                    <Button
+                        size="medium"
+                        variant={collapsedList[index] ? "text" : "elevated"}
+                        borderStyle="semi"
+                        onClick={() => handleClickCollapseList(index)}
+                    >
+                        {item.name}
+                        <Spacer />
+                    </Button>
+                    <List
+                        collapsed={collapsedList[index]}
+                        direction="column"
+                        style={{ marginInlineStart: "2rem" }}
+                    >
+                        {item.navListItems}
+                        <div style={{ height: "2rem" }} />
+                    </List>
+                </ListItem>
+            ))}
+        </List>
+    );
+}
+
+function NavListItemsQuran(props: {
+    config: QuranConfigProps;
+    setConfig: any;
+}) {
     const surahListFetch = useFetch<SurahInListProps[]>(
         `${process.env.REACT_APP_API_URL}/surah?mushaf=hafs`,
         {
             method: "GET",
         }
     );
+
+    useEffect(() => {
+        surahListFetch.send();
+    }, []);
+
+    return (
+        <ListItem>
+            {surahListFetch.isResponseBodyReady ? (
+                <Select
+                    variant="filled"
+                    name="surahUUID"
+                    placeholder="Surah"
+                    defaultValue={
+                        props.config.surahUUID
+                            ? props.config.surahUUID
+                            : undefined
+                    }
+                    onChange={(e) =>
+                        props.setConfig({
+                            ...props.config,
+                            surahUUID: e.target.value,
+                        })
+                    }
+                >
+                    {surahListFetch.responseBody.map((surah) => (
+                        <option value={surah.uuid}>
+                            {surah.number + " - " + surah.name[0].arabic}
+                        </option>
+                    ))}
+                </Select>
+            ) : (
+                <Loading variant="dots" />
+            )}
+        </ListItem>
+    );
+}
+
+function NavListItemsArabicText(props: {
+    config: QuranConfigProps;
+    setConfig: any;
+}) {
+    return (
+        <>
+            <ListItem>
+                <Row style={{ height: "3.2rem" }}>
+                    <span>Show:</span>
+                    <Spacer />
+                    <input type="checkbox" name="showArabic" />
+                </Row>
+            </ListItem>
+            <ListItem>
+                <Row style={{ height: "3.2rem" }}>
+                    <span>Tajweed:</span>
+                    <Spacer />
+                    <input type="checkbox" name="showArabic" />
+                </Row>
+            </ListItem>
+            <ListItem>
+                <Select variant="filled" placeholder="Font:" disabled>
+                    <option value="1">FontName</option>
+                </Select>
+            </ListItem>
+        </>
+    );
+}
+
+function NavListItemsTranslation(props: {
+    config: QuranConfigProps;
+    setConfig: any;
+}) {
     const translationListFetch = useFetch<TranslationInListProps[]>(
         process.env.REACT_APP_API_URL + `/translation?mushaf=hafs`,
         {
@@ -55,7 +177,6 @@ export default function NavigationList(props: {
     );
 
     useEffect(() => {
-        surahListFetch.send();
         translationListFetch.send();
     }, []);
 
@@ -73,223 +194,56 @@ export default function NavigationList(props: {
     }, [translationListFetch.isResponseBodyReady]);
 
     return (
-        <List direction="column">
+        <>
             <ListItem>
-                <Button
-                    size="medium"
-                    borderStyle="semi"
-                    onClick={() => handleClickCollapseList(0)}
-                >
-                    Quran
+                <Row style={{ height: "3.2rem" }}>
+                    <span>Show:</span>
                     <Spacer />
-                </Button>
-                <List
-                    collapsed={collapsedList[0]}
-                    direction="column"
-                    style={{ paddingInlineStart: "2rem" }}
-                >
-                    <ListItem>
-                        {surahListFetch.isResponseBodyReady ? (
-                            <Select
-                                variant="filled"
-                                name="surahUUID"
-                                placeholder="Surah"
-                                defaultValue={
-                                    props.config.surahUUID
-                                        ? props.config.surahUUID
-                                        : undefined
-                                }
-                                onChange={(e) =>
-                                    props.setConfig({
-                                        ...props.config,
-                                        surahUUID: e.target.value,
-                                    })
-                                }
-                            >
-                                {surahListFetch.responseBody.map((surah) => (
-                                    <option value={surah.uuid}>
-                                        {surah.number +
-                                            " - " +
-                                            surah.name[0].arabic}
-                                    </option>
-                                ))}
-                            </Select>
-                        ) : (
-                            <Loading variant="dots" />
-                        )}
-                    </ListItem>
-                </List>
+                    <input
+                        type="checkbox"
+                        name="translationView"
+                        defaultChecked={props.config.translationView}
+                        onChange={(e) =>
+                            props.setConfig({
+                                ...props.config,
+                                translationView: e.target.checked,
+                            })
+                        }
+                    />
+                </Row>
             </ListItem>
-
-            <ListItem style={{ display: "none" }}>
-                <Button
-                    size="medium"
-                    borderStyle="semi"
-                    onClick={() => handleClickCollapseList(1)}
-                >
-                    Rsitation
-                </Button>
-                <List
-                    collapsed={collapsedList[1]}
-                    direction="column"
-                    style={{ paddingInlineStart: "2rem" }}
-                >
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span> Gari:</span>
-                            <Spacer />
-                            <select>
-                                <option value="1">Al-Afasy</option>
-                            </select>
-                        </Row>
-                    </ListItem>
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span>Play:</span>
-                            <Spacer />
-                            <select>
-                                <option value="1">Countinues</option>
-                            </select>
-                        </Row>
-                    </ListItem>
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span>Delay:</span>
-                            <Spacer />
-                            <select>
-                                <option value="1">1 sec</option>
-                            </select>
-                        </Row>
-                    </ListItem>
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span>Repeat:</span>
-                            <Spacer />
-                            <select>
-                                <option value="1">1 time</option>
-                            </select>
-                        </Row>
-                    </ListItem>
-                </List>
-            </ListItem>
-
-            <ListItem style={{ display: "none" }}>
-                <Button
-                    size="medium"
-                    borderStyle="semi"
-                    onClick={() => handleClickCollapseList(2)}
-                >
-                    Arabic Text
-                </Button>
-                <List
-                    collapsed={collapsedList[2]}
-                    direction="column"
-                    style={{ paddingInlineStart: "2rem" }}
-                >
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span>Show:</span>
-                            <Spacer />
-                            <input type="checkbox" name="showArabic" />
-                        </Row>
-                    </ListItem>
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span>Tajweed:</span>
-                            <Spacer />
-                            <input type="checkbox" name="showArabic" />
-                        </Row>
-                    </ListItem>
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span>Font:</span>
-                            <Spacer />
-                            <select name="">
-                                <option value="">Hafs</option>
-                            </select>
-                        </Row>
-                    </ListItem>
-                    <ListItem>
-                        <Row style={{ height: "3.2rem" }}>
-                            <span> Test:</span>
-                            <Spacer />
-                            <select name="">
-                                <option value="">Uthmani</option>
-                            </select>
-                        </Row>
-                    </ListItem>
-                </List>
-            </ListItem>
-
             <ListItem>
-                <Button
-                    size="medium"
-                    borderStyle="semi"
-                    onClick={() => handleClickCollapseList(3)}
-                >
-                    Translation
-                </Button>
-                <List
-                    collapsed={collapsedList[3]}
-                    direction="column"
-                    style={{ paddingInlineStart: "2rem" }}
-                >
-                    <ListItem>
-                        <Stack>
-                            <Row style={{ height: "3.2rem" }}>
-                                <span>Show:</span>
-                                <Spacer />
-                                <input
-                                    type="checkbox"
-                                    name="translationView"
-                                    defaultChecked={
-                                        props.config.translationView
-                                    }
-                                    onChange={(e) =>
-                                        props.setConfig({
-                                            ...props.config,
-                                            translationView: e.target.checked,
-                                        })
-                                    }
-                                />
-                            </Row>
-                            {translationListFetch.isResponseBodyReady &&
-                            props.config.translationUUID ? (
-                                <Select
-                                    variant="filled"
-                                    placeholder="Translator"
-                                    defaultValue={
-                                        props.config.translationUUID
-                                            ? props.config.translationUUID
-                                            : undefined
-                                    }
-                                    onChange={(e) =>
-                                        props.setConfig({
-                                            ...props.config,
-                                            translationUUID: e.target.value,
-                                        })
-                                    }
-                                >
-                                    {translationListFetch.responseBody.map(
-                                        (translation) => (
-                                            <option value={translation.uuid}>
-                                                {getLangNameFromCode(
-                                                    translation.language
-                                                ) +
-                                                    " - " +
-                                                    translation.translator
-                                                        .username}
-                                            </option>
-                                        )
-                                    )}
-                                </Select>
-                            ) : (
-                                <Loading variant="dots" />
-                            )}
-                        </Stack>
-                    </ListItem>
-                </List>
+                {translationListFetch.isResponseBodyReady &&
+                props.config.translationUUID ? (
+                    <Select
+                        variant="filled"
+                        placeholder="Translation"
+                        defaultValue={
+                            props.config.translationUUID
+                                ? props.config.translationUUID
+                                : undefined
+                        }
+                        onChange={(e) =>
+                            props.setConfig({
+                                ...props.config,
+                                translationUUID: e.target.value,
+                            })
+                        }
+                    >
+                        {translationListFetch.responseBody.map(
+                            (translation) => (
+                                <option value={translation.uuid}>
+                                    {getLangNameFromCode(translation.language) +
+                                        " - " +
+                                        translation.translator.username}
+                                </option>
+                            )
+                        )}
+                    </Select>
+                ) : (
+                    <Loading variant="dots" />
+                )}
             </ListItem>
-        </List>
+        </>
     );
 }
